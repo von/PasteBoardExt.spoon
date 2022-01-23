d47a79dcd616f460eddeb7a00ec6dfc7e887aa1f
Von Welch <von@vwelch.com>
2022-01-23 10:44:23 -0500

    pbedit.sh: try hard to find mvim

--- bin/pbedit.sh
+++ bin/pbedit.sh
@@ -3,8 +3,26 @@
 
 set -o errexit  # Exit on error
 
-# Default editor is MacVim as installed by Homebrew
-editor=${EDITOR:-/usr/local/bin/mvim}
+if test -n "${EDITOR}" ; then
+  editor="${EDITOR}"
+else
+  # Default editor is MacVim as installed by Homebrew
+  for mvim in \
+    /usr/local/bin/mvim \
+    /opt/homebrew/bin/mvim \
+    /Applications/MacVim.app/Contents/bin/mvim \
+    ; do
+    if test -x "${mvim}" ; then
+      editor="${mvim}"
+      break
+    fi
+  done
+fi
+
+if test -z "${editor}" ; then
+  echo "Macvim (mvim) not found." 1>&2
+  exit 1
+fi
 
 # Use --nofork for mvim
 if [[ ${editor} == *mvim ]] ; then
